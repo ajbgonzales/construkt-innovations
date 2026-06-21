de975afc2fec0b75bf3976440e36e7fd3594e6e0
Gonzales, Aidrhiz Jhan <aidrhizgonzales@gmail.com>
2026-06-21 20:48:05 +0800

    chore: import allowed hosts from env file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
+import os
+
 from api.routes import router as api_router
+
+from dotenv import load_dotenv
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://construkt-innovations-vih6.onrender.com"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS").split(","),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
