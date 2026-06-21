c6eef371f0cd18655afe26ab995525974ca84c72
Gonzales, Aidrhiz Jhan <aidrhizgonzales@gmail.com>
2026-06-21 20:46:16 +0800

    chore: import allowed hosts from env file

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://construkt-innovations-vih6.onrender.com"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
